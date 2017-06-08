da20581a286accb43fb5a29962f0fe8d9e68cb85
github username <alexsambvani@gmail.com>
2017-06-08 17:06:18 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,72 @@ require 'pry'
 class String
 
   def sentence?
-    
+
+    return self.end_with?(".")
+
   end
 
   def question?
+
+    return self.end_with?("?")
 
   end
 
   def exclamation?
 
+    return self.end_with?("!")
+
   end
 
   def count_sentences
 
+    sentence_array = [self]
+
+    sentence_array = remove_periods(sentence_array)
+    sentence_array = remove_question(sentence_array)
+    sentence_array = remove_exclamation(sentence_array)
+    sentence_array.delete("")
+    return sentence_array.length
+
   end
+
+  def remove_periods(sentence_array)
+    while sentence_array.join.include?(".") do
+      sentence_array.map! do |sentence|
+        if sentence.include?(".")
+            sentence.split(".")
+        else
+          sentence
+        end
+      end.flatten!
+    end
+    return sentence_array
+  end
+
+  def remove_question(sentence_array)
+    while sentence_array.join.include?("?") do
+      sentence_array.map! do |sentence|
+        if sentence.include?("?")
+            sentence.split("?")
+        else
+          sentence
+        end
+      end.flatten!
+    end
+    return sentence_array
+  end
+
+  def remove_exclamation(sentence_array)
+    while sentence_array.join.include?("!") do
+      sentence_array.map! do |sentence|
+        if sentence.include?("!")
+            sentence.split("!")
+        else
+          sentence
+        end
+      end.flatten!
+    end
+    return sentence_array
+  end
+
 end
